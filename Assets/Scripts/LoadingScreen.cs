2960f2be99cb7b715e93072eb5e8c3f9175b1b89
Laurie Trichet <laurie.trichet@gmail.com>
2020-04-27 16:44:39 +1000

    update unity version
    replace fonts

--- Assets/Scripts/LoadingScreen.cs
+++ Assets/Scripts/LoadingScreen.cs
@@ -5,27 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class LoadingScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private int currentSceneIndex = 0;
+
     void Start()
     {
-        LoadAssets();
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex == 0)
+        {
+            LoadAssets();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void LoadAssets()
     {
         StartCoroutine(LoadAssetCoroutine());
-
     }
 
     IEnumerator LoadAssetCoroutine()
     {
         yield return new WaitForSecondsRealtime(2.0f);
-        SceneManager.LoadScene(1);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex+1);
     }
 }
+ 
