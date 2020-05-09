c5f08961035761af5d907235866bcfb111fcea33
Laurie Trichet <laurie.trichet@gmail.com>
2020-05-09 14:56:42 +1000

    add spawning of attacke

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range(0.1f, 3.0f)]
+    [SerializeField] float speed = 1.0f;  
+    [Range(0.5f, 1.5f)]
+    [SerializeField] float defaultAnimationSpeed = 1.13f;
+
+    
+
+    private Animator animator = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        animator.speed = defaultAnimationSpeed * speed;
+    }
+}
---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] int Min = 1;
+    [SerializeField] int Max = 5;
+    [SerializeField] bool shouldSpawn = false;
+
+    [SerializeField] GameObject prefab = null;
+
+    IEnumerator Start()
+    {
+        while (shouldSpawn)
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(Min, Max));
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        Instantiate(prefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
