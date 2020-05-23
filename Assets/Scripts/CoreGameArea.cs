ea33306d1758ddcffa2b28cfef42ffe13058c274
Laurie Trichet <laurie.trichet@gmail.com>
2020-05-23 12:01:45 +1000

    snap defender to position on grid

---
+++ Assets/Scripts/CoreGameArea.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreGameArea : MonoBehaviour
+{
+
+    private DefenderSpawner defenderSpawner = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        defenderSpawner = GetComponent<DefenderSpawner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        
+    }
+
+    private void OnMouseDown()
+    {
+        var worldSpaceCoordinate = Input.mousePosition;
+        defenderSpawner.SpawnDefender(GetFormattedPosition(worldSpaceCoordinate));
+    }
+
+    private Vector3 GetFormattedPosition(Vector3 position)
+    {
+        var localMousePosition = Camera.main.ScreenToWorldPoint(position);
+        Debug.Log(localMousePosition);
+        var x = Mathf.RoundToInt(localMousePosition.x) ;
+        var y = Mathf.RoundToInt(localMousePosition.y) ;
+        localMousePosition = new Vector3(x, y, 0);
+        Debug.Log(localMousePosition);
+        return localMousePosition;
+    }
+}
---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+
+    [SerializeField] GameObject defenderPrefab = null;
+
+    public void SpawnDefender(Vector3 position)
+    {
+        Instantiate(defenderPrefab, position, transform.rotation);
+    }
+
+
+}
