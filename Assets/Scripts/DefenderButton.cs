939000bfc575185b2cee7359fe5cc5bc405e2a38
Laurie Trichet <laurie.trichet@gmail.com>
2020-05-23 20:07:03 +1000

    add denfender buttons

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class DefenderButton : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer = null;
+    private DefenderButton[] siblings = null;
+    private List<SpriteRenderer> siblingRenderers = null;
+    private Color color;
+    void Start()
+    {
+        siblings = FindObjectsOfType<DefenderButton>();
+        siblingRenderers = siblings.Select(sibling => sibling.gameObject.GetComponent<SpriteRenderer>()).ToList();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        color = spriteRenderer.color;
+    }
+
+    private void OnMouseDown()
+    {
+        siblingRenderers.ForEach(siblingRenderer => siblingRenderer.color = color);
+
+        spriteRenderer.color = Color.white;
+    }
+}
