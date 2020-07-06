4fa48f8fc2f47db41286817aca3673122f832f22
Laurie Trichet <laurie.trichet@gmail.com>
2020-07-06 16:28:45 +1000

    add win sfx

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Linq;
+using System.Media;
 using UnityEngine;
 
 public class LevelController : MonoBehaviour
@@ -10,15 +11,18 @@ public class LevelController : MonoBehaviour
     [SerializeField] GameObject spawnerContainer = null;
 
     [SerializeField] LoadingScreen loadingScreen = null;
+    AudioSource winSFXSource = null;
     [SerializeField] TMPro.TMP_Text winText = null;
     bool isObservingEnemies = false;
     private Coroutine onEnemiesDeadCoroutine;
     private AttackerSpawner[] attackerSpawners;
 
+
     void Start()
     {
         gameTimer.HasTerminated = OnTimerTerminated;
         attackerSpawners = FindObjectsOfType<AttackerSpawner>();
+        winSFXSource = GetComponent<AudioSource>();
     }
 
     void OnTimerTerminated()
@@ -50,6 +54,7 @@ public class LevelController : MonoBehaviour
     IEnumerator OnEnemiesDead()
     {
         winText.enabled = true;
+        winSFXSource.Play();
         yield return new WaitForSeconds(5);
         loadingScreen.LoadNextScene();
         StopCoroutine(onEnemiesDeadCoroutine);
